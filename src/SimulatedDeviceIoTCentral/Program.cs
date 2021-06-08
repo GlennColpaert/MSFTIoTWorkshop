3e88365e96be6146afb8c6f3f690b60d786be05a
GlennColpaert <glenn_colpaert@hotmail.com>
2021-06-08 15:23:10 +0200

    add content

---
+++ src/SimulatedDeviceIoTCentral/Program.cs
@@ -0,0 +1,105 @@
+﻿
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Provisioning.Client;
+using Microsoft.Azure.Devices.Provisioning.Client.Transport;
+using Microsoft.Azure.Devices.Shared;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Text.Json;
+using Newtonsoft.Json;
+
+namespace SimulatedDevice
+{
+    class Program
+    {
+        private static DeviceClient s_deviceClient;
+        private readonly static string s_myDeviceId = "";
+
+        private static async Task Main()
+        {
+
+            using var security = new SecurityProviderSymmetricKey(s_myDeviceId,
+                 "", null);
+
+            ProvisioningDeviceClient provClient = ProvisioningDeviceClient.Create(
+                "global.azure-devices-provisioning.net",
+                "",
+                security,
+                new ProvisioningTransportHandlerMqtt());
+
+            Console.WriteLine($"Initialized for registration Id {security.GetRegistrationID()}.");
+
+            Console.WriteLine("Registering with the device provisioning service...");
+            DeviceRegistrationResult result = await provClient.RegisterAsync();
+
+            Console.WriteLine($"Registration status: {result.Status}.");
+            if (result.Status != ProvisioningRegistrationStatusType.Assigned)
+            {
+                Console.WriteLine($"Registration status did not assign a hub, so exiting this sample.");
+                return;
+            }
+
+            Console.WriteLine($"Device {result.DeviceId} registered to {result.AssignedHub}.");
+
+            Console.WriteLine("Creating symmetric key authentication for IoT Hub...");
+            IAuthenticationMethod auth = new DeviceAuthenticationWithRegistrySymmetricKey(
+                result.DeviceId,
+                security.GetPrimaryKey());
+
+            Console.WriteLine($"Testing the provisioned device with IoT Hub...");
+
+            s_deviceClient = DeviceClient.Create(result.AssignedHub, auth, TransportType.Mqtt);
+
+            using var cts = new CancellationTokenSource();
+            var messages = SendDeviceToCloudMessagesAsync(cts.Token);
+            Console.WriteLine("Press the Enter key to stop.");
+            Console.ReadLine();
+            await s_deviceClient.CloseAsync(cts.Token);
+            cts.Cancel();
+            await messages;
+
+        }
+
+        /// <summary> 
+        /// Send message to the Iot hub. This generates the object to be sent to the hub in the message.
+        /// </summary>
+        private static async Task SendDeviceToCloudMessagesAsync(CancellationToken token)
+        {
+            double minTemperature = 20;
+            double minHumidity = 60;
+            Random rand = new Random();
+
+            while (!token.IsCancellationRequested)
+            {
+                double currentTemperature = minTemperature + rand.NextDouble() * 15;
+                double currentHumidity = minHumidity + rand.NextDouble() * 20;
+
+                var telemetryDataPoint = new
+                {
+                    DeviceId = s_myDeviceId,
+                    Temperature = currentTemperature,
+                    Humidity = currentHumidity
+                };
+                // serialize the telemetry data and convert it to JSON.
+                var telemetryDataString = JsonConvert.SerializeObject(telemetryDataPoint);
+
+                 using var message = new Message(Encoding.UTF8.GetBytes(telemetryDataString))
+                {
+                    ContentEncoding = "utf-8",
+                    ContentType = "application/json",
+                };
+
+                // Submit the message to the hub.
+                await s_deviceClient.SendEventAsync(message);
+
+                // Print out the message.
+                Console.WriteLine("{0} > Sent message: {1}", DateTime.UtcNow, telemetryDataString);
+
+                await Task.Delay(1000);
+            }
+        }      
+    }
+}
